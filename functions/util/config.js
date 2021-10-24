83c2d0ffdc4db818b35f368fdc2f638fa9f846bc
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-10-24 18:24:54 +0200

    cleared config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    apiKey: "AIzaSyAqFt-2KvWR0xfCQou35rC6gWg7JvpPins",
-    authDomain: "medi-app-1-715b8.firebaseapp.com",
-    // databaseURL: "........",
-    projectId: "medi-app-1-715b8",
-    storageBucket: "medi-app-1-715b8.appspot.com",
-    messagingSenderId: "517345081734",
-    appId: "1:517345081734:web:e04784d74448b07ea79407",
-    // measurementId: ".......",
-};
+
